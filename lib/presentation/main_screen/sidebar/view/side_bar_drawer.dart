010878c2e51735cc3fa699707fd0467e1c5c2770
repsajanboy <janboym@gmail.com>
2022-08-16 13:56:11 +0800

    Sidebar categories UI design

--- lib/presentation/main_screen/sidebar/view/side_bar_drawer.dart
+++ lib/presentation/main_screen/sidebar/view/side_bar_drawer.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../data/category/category.dart';
 import '../../../../repositories/category_repository.dart';
+import '../../../../utils/color_picker_items.dart';
 import '../../category/category.dart';
 import '../sidebar.dart';
 
@@ -17,120 +18,108 @@ class SideBarDrawer extends StatelessWidget {
       )..add(CategoriesFetched()),
       child: Drawer(
         backgroundColor: Colors.grey[900],
-        child: ListView(
-          children: [
-            BlocBuilder<SidebarBloc, SidebarState>(
-              builder: (context, state) {
-                switch (state.status) {
-                  case SideBarStatus.success:
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      physics: const ClampingScrollPhysics(),
-                      itemCount: state.categories.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return _categoriesItem(
-                            context, state.categories[index]);
-                      },
-                    );
-                  default:
-                    return const SizedBox();
-                }
-              },
-            ),
-            // ListTile(
-            //   leading: Container(
-            //     decoration: const BoxDecoration(
-            //       color: Color(0xFFFF9F0A),
-            //       borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            //     ),
-            //     padding: const EdgeInsets.all(10.0),
-            //     child: const Text(
-            //       'F',
-            //       style: TextStyle(color: Colors.white),
-            //     ),
-            //   ),
-            //   title: const Text(
-            //     'Family',
-            //     style: TextStyle(color: Colors.white70),
-            //   ),
-            // ),
-            // ListTile(
-            //   leading: Container(
-            //     decoration: const BoxDecoration(
-            //       color: Color(0xFF0A84FF),
-            //       borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            //     ),
-            //     padding: const EdgeInsets.all(10.0),
-            //     child: const Text(
-            //       'S',
-            //       style: TextStyle(color: Colors.white),
-            //     ),
-            //   ),
-            //   title: const Text(
-            //     'Special',
-            //     style: TextStyle(color: Colors.white70),
-            //   ),
-            // ),
-            // ListTile(
-            //   leading: Container(
-            //     decoration: const BoxDecoration(
-            //       color: Color(0xFFFF375F),
-            //       borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            //     ),
-            //     padding: const EdgeInsets.all(10.0),
-            //     child: const Text(
-            //       'P',
-            //       style: TextStyle(color: Colors.white),
-            //     ),
-            //   ),
-            //   title: const Text(
-            //     'Personal',
-            //     style: TextStyle(color: Colors.white70),
-            //   ),
-            // ),
-            ListTile(
-              leading: const Icon(
-                Icons.add,
-                color: Colors.white70,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: ListView(
+            children: [
+              BlocBuilder<SidebarBloc, SidebarState>(
+                builder: (context, state) {
+                  switch (state.status) {
+                    case SideBarStatus.success:
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: const ClampingScrollPhysics(),
+                        itemCount: state.categories.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return _categoriesItem(
+                              context, state.categories[index]);
+                        },
+                      );
+                    default:
+                      return const SizedBox();
+                  }
+                },
               ),
-              title: const Text(
-                'Add Category',
-                style: TextStyle(color: Colors.white70),
-              ),
-              onTap: () {
-                showModalBottomSheet<void>(
-                  context: context,
-                  isScrollControlled: true,
+              ListTile(
+                leading: Card(
                   shape: const RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(30.0))),
-                  builder: (BuildContext context) =>
-                      const CategoryBottomSheet(),
-                );
-              },
-            )
-          ],
+                      borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                  elevation: 5.0,
+                  color: Colors.grey[800],
+                  child: const SizedBox(
+                    width: 50.0,
+                    height: 50.0,
+                    child: Center(
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white70,
+                      ),
+                    ),
+                  ),
+                ),
+                title: const Text(
+                  'Add Category',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontFamily: 'Nunito',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                onTap: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    isScrollControlled: true,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(30.0))),
+                    builder: (BuildContext context) =>
+                        const CategoryBottomSheet(),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _categoriesItem(BuildContext context, Category category) {
-    return ListTile(
-      leading: Container(
-        decoration: const BoxDecoration(
-          color: Color(0xFFBF5AF2),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    final _color = colorPickerItems.firstWhere((e) => e.id == category.theme);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: ListTile(
+        leading: Card(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          elevation: 5.0,
+          color: Colors.grey[800],
+          child: SizedBox(
+            width: 50.0,
+            height: 50.0,
+            child: Center(
+              child: Text(
+                category.name.substring(0, 1),
+                style: TextStyle(
+                  fontFamily: 'Open Sans',
+                  color: _color.colors,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22.0,
+                ),
+              ),
+            ),
+          ),
         ),
-        padding: const EdgeInsets.all(10.0),
-        child: Text(
-          category.name.substring(0, 1),
-          style: const TextStyle(color: Colors.white),
+        title: Text(
+          category.name,
+          style: const TextStyle(
+            color: Colors.white70,
+            fontFamily: 'Nunito',
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+          ),
         ),
-      ),
-      title: Text(
-        category.name,
-        style: const TextStyle(color: Colors.white70),
       ),
     );
   }
